b53b6ad7368d8d3b569e1c5b8a2aa5ac6e94a2bd
Oleksandr Holan <game.play.for.alex@gmail.com>
2025-02-06 22:23:18 +0200

    Add NewGitLabConfig func

--- pkg/config/configs.go
+++ pkg/config/configs.go
@@ -1,7 +1,36 @@
 package configs
 
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
 type GitLab struct {
-	GitlabURL   string `json:"gitlab_url"`
-	AccessToken string `json:"access_token"`
-	MainGroupID int    `json:"main_group_id"`
+	GitlabURL   string `json:"gitlab_url" env-required:"true" env:"GITLAB_URL"`
+	AccessToken string `json:"access_token" env-required:"true" env:"ACCESS_TOKEN"`
+	MainGroupID int    `json:"main_group_id" env-required:"true" env:"MAIN_GROUP_ID"`
+}
+
+func NewGitLabConfig() (*GitLab, error) {
+	configPath := filepath.Join(".", "config.json")
+	cfg := &GitLab{}
+
+	err := cleanenv.ReadConfig(configPath, cfg)
+	if err != nil {
+		return nil, fmt.Errorf("config error: %w", err)
+	}
+
+	err = cleanenv.ReadEnv(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	if !strings.HasPrefix(cfg.GitlabURL, "https://") {
+		return nil, fmt.Errorf("gitlab_url must use HTTPS (e.g., https://gitlab.example.com)")
+	}
+
+	return cfg, nil
 }
